d2b8880ee7ef0c90a9d2eb5948f39a86d30973be
Ian <ianlloydwilkinson@gmail.com>
2021-01-29 17:18:36 -0700

    cloudinary project done, request show on user page done

--- client/src/Hooks/useRequest.js
+++ client/src/Hooks/useRequest.js
@@ -2,7 +2,8 @@ import Axios from "axios";
 import { useState } from "react";
 
 const useRequest = (project, id) => {
-  const [requestStatus, setRequestStatus] = useState("none");
+  const [requestStatus, setRequestStatus] = useState("Join");
+  const [myRequests, setMyRequests] = useState([]);
 
 
   const checkRequests = (project, id) => {
@@ -37,10 +38,67 @@ const useRequest = (project, id) => {
       });
   };
 
+  const getTheseRequests = async (user_id) => {
+    try {
+      let res = await Axios.get(`/api/requests/${user_id}/pending-with-names`);
+      let dummyrequests = []
+      res.data.forEach(r => {
+        if (r.user_id == user_id) {
+          dummyrequests.push(r)
+        }
+        else {
+          return
+        }
+      })
+      dummyrequests.sort((a,b) => {
+        let fa = a.title.toLowerCase();
+        let fb = b.title.toLowerCase();
+        if (fa < fb) {
+          return -1;
+        }
+        if (fa > fb) {
+            return 1;
+        }
+        return 0;
+      })
+      setMyRequests(dummyrequests)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const denyRequest = async (project_id, request_id) => {
+    try {
+      let res = await Axios.delete(`/api/projects/${project_id}/requests/${request_id}`)
+      console.log(res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const acceptRequest = async (project_id, request_id, incoming_user_id) => {
+    try {
+      let res = await Axios.put(`/api/projects/${project_id}/requests/${request_id}`,
+      {
+        project_id: project_id,
+        user_id: incoming_user_id,
+        contributor: true,
+      })
+      console.log(res)
+    }
+    catch (err){
+      console.log(err)
+    }
+  }
+
   return {
     sendRequest,
     checkRequests,
     requestStatus,
+    getTheseRequests,
+    myRequests,
+    denyRequest,
+    acceptRequest,
   };
 };
 
