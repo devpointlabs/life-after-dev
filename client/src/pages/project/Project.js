a310e98f931a4f505ec23f9378e63a0236999f07
Taylor Collins <taylorcc33@gmail.com>
2021-02-01 16:11:09 -0700

    merge

--- client/src/pages/project/Project.js
+++ client/src/pages/project/Project.js
@@ -88,8 +88,15 @@ const Project = (props) => {
     }
   };
 
+  const togglePic = () => {
+    setSeen(!seen);
+  };
+
   return (
     <Wrapper>
+      {seen ? (
+        <ProjectPicModal toggle={togglePic} user={user} project={project} />
+      ) : null}
       <div className="Project_Title">
         <Link to={`/user/${owner.id}`}>
           <h2>
@@ -104,23 +111,20 @@ const Project = (props) => {
         {renderDeleteButton()}
         <div className="Project_Image">
           <img className="project_image" src={project?.picture} />
-          {renderRequestAction()}
+          <Button color="black">Join</Button>
+          <button type="button" onClick={togglePic}>
+            Change Picture
+          </button>
           <div className="description">
             <p>{project?.description}</p>
           </div>
           <div className="links">
-            <a
-              href={"https://github.com/devpointlabs/life-after-dev"}
-              onClick="console.log('The link was clicked.'); return false"
-            >
-              Github
+            <a href={`http://${project?.github_link}`} target="_blank">
+              <img className="Github" src={githubicon}></img>
             </a>
             <br />
-            <a
-              href={project?.live_link}
-              onClick="console.log('The link was clicked.'); return false"
-            >
-              Live_Link
+            <a href={`http://${project?.live_link}`} target="_blank">
+              <img className="LiveLink" src={livelink}></img>
             </a>
           </div>
         </div>
