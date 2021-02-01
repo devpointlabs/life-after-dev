315e5b34b5702f8dc8d7e9b91609a745241ee098
Taylor Collins <taylorcc33@gmail.com>
2021-02-01 15:52:12 -0700

    merge master

--- client/src/pages/project/Project.js
+++ client/src/pages/project/Project.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./style.css";
 import Comments from "./Comments";
 import styled from "styled-components";
@@ -9,6 +9,9 @@ import githubicon from "../../icons/githubicon.png";
 import livelink from "../../icons/livelink.png";
 import { CommentSection, Wrapper } from "../../styles/ProjectShowStyle";
 import ProjectPicModal from "./ProjectPicModal";
+import EditProjectModal from "../../components/EditProjectModal";
+import { Button, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const Project = (props) => {
   const [project, setProject] = useState(null);
@@ -18,21 +21,55 @@ const Project = (props) => {
   const [seen, setSeen] = useState(false);
 
   useEffect(() => {
-    getProject();
+    getProjectData();
   }, []);
 
-  const getProject = async () => {
+  const getProjectData = async () => {
     try {
       let res = await axios.get(`/api/projects/${props.match.params.id}`);
       setProject(res.data);
-      console.log(res);
+      console.log("project", res);
+      let ownerRes = await axios.get(`/api/users/${res.data.user_id}`);
+      setOwner(ownerRes.data);
+      console.log("owner", ownerRes);
     } catch (err) {
-      alert(err);
+      console.log(err);
     }
   };
 
-  const togglePic = () => {
-    setSeen(!seen);
+  const updateProjects = (project) => {
+    const updatedProject = project;
+    setProject(updatedProject);
+  };
+
+  const deleteProject = (id) => {
+    axios
+      .delete(`/api/users/${owner.id}/projects/${id}`)
+      .then((res) => {
+        console.log("project deleted", res);
+        props.history.push(`/user/${owner.id}`);
+      })
+      .catch((err) => {
+        console.log("delete project error", err);
+      });
+  };
+
+  const renderEditButton = () => {
+    if (user?.id == owner.id) {
+      return (
+        <EditProjectModal project={project} updateProjects={updateProjects} />
+      );
+    }
+  };
+
+  const renderDeleteButton = () => {
+    if (user?.id == owner.id) {
+      return (
+        <Button onClick={() => deleteProject(project.id)} color="red">
+          Delete
+        </Button>
+      );
+    }
   };
 
   const renderRequestAction = () => {
@@ -53,9 +90,6 @@ const Project = (props) => {
 
   return (
     <Wrapper>
-      {seen ? (
-        <ProjectPicModal toggle={togglePic} user={user} project={project} />
-      ) : null}
       <div className="Project_Title">
         <Link to={`/user/${owner.id}`}>
           <h2>
@@ -71,20 +105,22 @@ const Project = (props) => {
         <div className="Project_Image">
           <img className="project_image" src={project?.picture} />
           {renderRequestAction()}
-          <Button color="black">Join</Button>
-          <button type="button" onClick={togglePic}>
-            Change Picture
-          </button>
           <div className="description">
             <p>{project?.description}</p>
           </div>
           <div className="links">
-            <a href={`http://${project?.github_link}`} target="_blank">
-              <img className="Github" src={githubicon}></img>
+            <a
+              href={"https://github.com/devpointlabs/life-after-dev"}
+              onClick="console.log('The link was clicked.'); return false"
+            >
+              Github
             </a>
             <br />
-            <a href={`http://${project?.live_link}`} target="_blank">
-              <img className="LiveLink" src={livelink}></img>
+            <a
+              href={project?.live_link}
+              onClick="console.log('The link was clicked.'); return false"
+            >
+              Live_Link
             </a>
           </div>
         </div>
@@ -95,18 +131,5 @@ const Project = (props) => {
     </Wrapper>
   );
 };
-
-// const CommentSection = styled.div`
-//   background: #1e2021;
-//   width: 300px;
-//   height: 100vh;
-//   padding: 2rem;
-//   flex-shrink: 0;
-// `;
-
-// const Wrapper = styled.div`
-//   display: flex;
-//   justify-content: center;
-// `;
 
 export default Project;
