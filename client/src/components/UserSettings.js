ed5ac793884b4d5e020622b22253557b5c08e5f3
Taylor Collins <taylorcc33@gmail.com>
2021-02-03 13:28:58 -0700

    styled half of settings page

--- client/src/components/UserSettings.js
+++ client/src/components/UserSettings.js
@@ -2,6 +2,18 @@ import Axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 import { AuthContext } from "../providers/AuthProvider";
+import {
+  EditPicButton,
+  FormDiv,
+  HeaderDiv,
+  PageHeader,
+  ProfilePicDiv,
+  ProfilePicEditDiv,
+  ProfilePicText,
+  SubHeadDiv,
+  UserInfoDiv,
+  Wrapper,
+} from "../styles/UserSettingsStyle";
 import ProfilePicSetting from "./ProfilePicSetting";
 
 const UserSettings = ({ match, history }) => {
@@ -43,70 +55,73 @@ const UserSettings = ({ match, history }) => {
   };
 
   return (
-    <div>
-      {seen ? (
-        <ProfilePicSetting toggle={togglePic} user={user} history={history} />
-      ) : null}
-      <h1>User Settings</h1>
-      <div>
-        <img className="userpic" src={user?.image} />
-        <Button onClick={togglePic}>Edit Picture</Button>
-      </div>
-      <Form onSubmit={handleSubmit}>
-        <Button>Save Profile</Button>
-        <br />
-        <br />
-        <Form.Input
-          label="First Name"
-          placeholder="First Name"
-          name="firstname"
-          value={userData.firstname}
-          onChange={handleChange}
-        />
-        <Form.Input
-          label="Last Name"
-          placeholder="Last Name"
-          name="lastname"
-          value={userData.lastname}
-          onChange={handleChange}
-        />
-        {/* <Form.Input
-          label="Image"
-          placeholder="Image"
-          name="image"
-          value={userData.image}
-          onChange={handleChange}
-        /> */}
-        <Form.Input
-          label="GitHub"
-          placeholder="GitHub Link"
-          name="github_link"
-          value={userData.github_link}
-          onChange={handleChange}
-        />
-        <Form.Input
-          label="Personal Site"
-          placeholder="Personal Site URL"
-          name="personal_site"
-          value={userData.personal_site}
-          onChange={handleChange}
-        />
-        <Form.Input
-          label="Linkedin"
-          placeholder="Linkedin"
-          name="linkedin_link"
-          value={userData.linkedin_link}
-          onChange={handleChange}
-        />
-        <Form.Input
-          label="Tag"
-          placeholder="Tag"
-          name="tag"
-          value={userData.tag}
-          onChange={handleChange}
-        />
-      </Form>
-    </div>
+    <Wrapper>
+      <FormDiv>
+        {seen ? (
+          <ProfilePicSetting toggle={togglePic} user={user} history={history} />
+        ) : null}
+        <HeaderDiv>
+          <PageHeader>Account Information</PageHeader>
+        </HeaderDiv>
+        <SubHeadDiv>
+          <ProfilePicDiv>
+            <img className="userpic" src={user?.image} />
+          </ProfilePicDiv>
+          <ProfilePicEditDiv>
+            <ProfilePicText>Profile Picture</ProfilePicText>
+            <EditPicButton onClick={togglePic}>Replace</EditPicButton>
+          </ProfilePicEditDiv>
+        </SubHeadDiv>
+        <Form onSubmit={handleSubmit}>
+          <Button>Save Profile</Button>
+          <br />
+          <br />
+          <Form.Input
+            label="FIRST NAME"
+            placeholder="First Name"
+            name="firstname"
+            value={userData.firstname}
+            onChange={handleChange}
+          />
+          <Form.Input
+            label="LAST NAME"
+            placeholder="Last Name"
+            name="lastname"
+            value={userData.lastname}
+            onChange={handleChange}
+          />
+          <Form.Input
+            label="GITHUB LINK"
+            placeholder="GitHub Link"
+            name="github_link"
+            value={userData.github_link}
+            onChange={handleChange}
+          />
+          <Form.Input
+            label="PERSONAL SITE "
+            placeholder="Personal Site URL"
+            name="personal_site"
+            value={userData.personal_site}
+            onChange={handleChange}
+          />
+          <Form.Input
+            label="LINKEDIN"
+            placeholder="Linkedin"
+            name="linkedin_link"
+            value={userData.linkedin_link}
+            onChange={handleChange}
+          />
+          <Form.Input
+            label="TAG"
+            placeholder="Tag"
+            name="tag"
+            value={userData.tag}
+            onChange={handleChange}
+          />
+        </Form>
+      </FormDiv>
+      <UserInfoDiv></UserInfoDiv>
+    </Wrapper>
   );
 };
 
